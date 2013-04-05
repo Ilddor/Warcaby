fee0127641809c838da8e4aaa3dc02c638ef72ba
Kamil Kowalski <kamilosxd678@gmail.com>
2013-04-05 23:31:32 +0200

    AI EventListener prototype added
    Added a prototype of EventListener in CBot

--- Warcaby/Bot.cpp
+++ Warcaby/Bot.cpp
@@ -1,8 +1,34 @@
 #include "Bot.h"
+#include <string>
+#include <iostream>
+#include "Misc.h"
+
+void CBot::update(std::string eventName, EPieceColor color)
+{
+	if(eventName == "turn" && m_color == color)
+	{
+		  std::cout << "I think I should make a move now" << std::endl;
+		  //Here we proceed with magic such as getting board, thinking of best move and killing the other motherfucker
+	}
+	else if(eventName == "gameEnd")
+	{
+		  if(m_color == color)
+		  {
+		  	  std::cout << "Yippie!" << std::endl;
+			  //Here we will boast about how we just killed that motherfucker
+		  }
+		  else
+		  {
+		  	  std::cout << "Well shit :(" << std::endl;
+			  //Here we will cry like a little girl and get suicidal thoughts ;-)
+		  }
+	}
+}
 
 
 CBot::CBot(void)
 {
+	 
 }
 
 
--- Warcaby/Bot.h
+++ Warcaby/Bot.h
@@ -1,9 +1,13 @@
 #pragma once
+#include "Misc.h"
+#include <string>
+
 class CBot
 {
+private:
+	EPieceColor m_color;
 public:
-
+	void update(std::string eventName, EPieceColor color);
 	CBot(void);
 	~CBot(void);
 };
-
